c537a90936161c2a0705c36d1c4345c2786afac0
hveidsten <hveidsten@gmail.com>
2018-04-24 15:14:43 +0200

    Backer opp det jeg har før jeg skal prøve noe merkelig.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Question extends Component {
+  render() {
+    return(
+      <div>
+      <h2>{this.props.category}</h2>
+      {this.props.questions.map(q => <div><h3>{q.heading}</h3> <p>{q.body}</p></div>)}
+</div>
+    );
+  }
+}
+
 class App extends Component {
 constructor(props){
   super(props);
   this.state={
-    values: []
+    data: []
   }
 }
 
 componentDidMount(){
-const url = "./api/values";
+const url = "https://5adf192bbf932f0014d11b7c.mockapi.io/kategorier";
 fetch(url)
-.then(response => response.text())
-.then(contents => this.setState({values: contents}))
+.then(response => response.json())
+.then(json => this.setState({data:json}))
 }
 
   render() {
     return (
       <div className="App">
-     <h3>{this.state.values}</h3>
+        {this.state.data.map((c, i) => <Question category={c.category} questions={c.questions} key={i}/>) }
       </div>
     );
   }
