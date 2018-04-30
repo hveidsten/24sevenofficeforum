1ca8ca77c661bf8b54293aa1761e54ee3e261ac5
hveidsten <hveidsten@gmail.com>
2018-04-30 11:40:38 +0200

    En fantastisk fancy utheving av aktiv side.

--- Frontend/src/Components/Sidebar.js
+++ Frontend/src/Components/Sidebar.js
@@ -6,10 +6,10 @@ class Sidebar extends Component{
     render(){
         return(
             <div className="Sidebar">
-            <NavLink to="/" >Hjem</NavLink>
+            <NavLink exact to="/" activeStyle={{ color: '#ffffff', fontWeight: 'bold'}}>Hjem</NavLink>
             <hr/>
             {this.props.categories.map(
-                (c, key) => {return <NavLink to={"/"+c.id} key={key} activeStyle={{ backgroundColor: '#6699bb'}}>{c.category}<hr/></NavLink>}
+                (c, key) => {return <NavLink to={"/"+c.id} key={key} activeStyle={{ color: '#ffffff', fontWeight: 'bold'}}>{c.category}<hr/></NavLink>}
                 
             )}
             
