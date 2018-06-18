370fa6e8a7ae9966e7b8d246ea9acce52b87210b
hveidsten <hveidsten@gmail.com>
2018-06-18 15:13:16 +0200

    Splittet opp og ryddet en komponent.

--- Frontend/src/Components/NewQuestion/NewQuestionComponent.js
+++ Frontend/src/Components/NewQuestion/NewQuestionComponent.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-export const NewQuestionComponent = () => {
-
-    
+export const NewQuestionComponent = ({handleSubmit, questionHeading, handleChange, questionBody,categories}) => {
+return(
+           <form onSubmit={handleSubmit}>
+       <label >Overskrift: </label>
+          <input type="text" name="questionHeading" value={questionHeading} onChange={handleChange} />
+          <br/>
+          <label>Spørsmål: </label>
+          <textarea rows="10" cols="25" name="questionBody" value={questionBody} onChange={handleChange} />
+          <br/>
+         <label>Kategori: </label>
+         <select size="10" name="categoryId" onInput={handleChange}>{categories.map((c,key) => { return <option key= {key} value={c.id}>{c.categoryName}</option>})}</select>
+        <input type="submit" value="Post" />
+      </form>
+);
 }
--- Frontend/src/Components/NewQuestion/NewQuestionContainer.js
+++ Frontend/src/Components/NewQuestion/NewQuestionContainer.js
@@ -3,11 +3,15 @@ import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {createPost} from '../../Actions/postActions';
 import { Redirect } from 'react-router';
+import {NewQuestionComponent} from './NewQuestionComponent';
 
 class NewQuestion extends Component{
     constructor(props) {
         super(props);
-        this.state = {questionHeading: '', questionBody: '', categoryId:this.props.activeCategory.id};
+        this.state = {questionHeading: '',
+                     questionBody: '', 
+                     categoryId:this.props.activeCategory?this.props.activeCategory.id:1
+                    };
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -36,19 +40,19 @@ class NewQuestion extends Component{
     render() {    
                return(
                  <div className="newQuestionForm">
-                 <form onSubmit={this.handleSubmit}>
-       <label >Overskrift: </label>
-          <input type="text" name="questionHeading" value={this.state.questionHeading} onChange={this.handleChange} />
-          <br/>
-          <label>Spørsmål: </label>
-          <textarea rows="10" cols="25" name="questionBody" value={this.state.questionBody} onChange={this.handleChange} />
-          <br/>
-         <label>Kategori: </label>
-         <select size="10" name="categoryId" onInput={this.handleChange}>{this.props.categories.map((c,key) => { return <option key= {key} value={c.id}>{c.categoryName}</option>})}</select>
-        <input type="submit" value="Post" />
-      </form>
-  
-      {this.props.post && this.props.post.hasBeenPosted && <Redirect to={this.props.categories.find(c => c.id===this.props.post.categoryId).categoryName.split(' ').join('_')+"/"+this.props.post.id} />}
+
+                  <NewQuestionComponent 
+                  handleSubmit={this.handleSubmit}
+                  questionHeading = {this.state.questionHeading}
+                  handleChange = {this.handleChange}
+                  questionBody = {this.state.questionBody}
+                  categories = {this.props.categories}
+                  />
+       
+                {/*Redirect om spørsmål er postet.*/}
+      {this.props.post && this.props.post.hasBeenPosted && 
+      <Redirect to={this.props.categories.find(c => c.id===this.props.post.categoryId).categoryName.replace(' ','_')+"/"+this.props.post.id} />}
+
       </div>
 
               );
