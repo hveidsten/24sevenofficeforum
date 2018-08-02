78bae57d29fd8ef3e52033e6861d2259ad8757ea
hveidsten <hveidsten@gmail.com>
2018-08-02 11:52:44 +0200

    Endret litt på nytt spørsmål-siden.

--- Frontend/src/Components/NewQuestion/NewQuestionComponent.js
+++ Frontend/src/Components/NewQuestion/NewQuestionComponent.js
@@ -2,22 +2,26 @@ import React from 'react';
 
 export const NewQuestionComponent = ({handleSubmit, questionHeading, handleChange, questionBody,categories}) => {
 return(
-           <form onSubmit={handleSubmit}>
+  <div> 
+        <h2>Nytt spørsmål</h2>
+           <form onSubmit={handleSubmit} >
 
              <label >Overskrift: </label>
-            <input type="text" name="questionHeading" value={questionHeading} onChange={handleChange} />
+            <input size="50" type="text" name="questionHeading" value={questionHeading} onChange={handleChange} style={{marginBottom:"2em"}} />
               <br/>
 
-             <label>Spørsmål: </label>
-             <textarea rows="10" cols="25" name="questionBody" value={questionBody} onChange={handleChange} />
-             <br/>
+             <label for="questionBody">Spørsmål: </label>
+             <textarea rows="20" cols="55" name="questionBody" value={questionBody} onChange={handleChange} style={{marginBottom:"2em"}} />
+             <br/> 
 
              <label>Kategori: </label>
-              <select size="10" name="categoryId" onChange={handleChange}>
-              {categories.map((c,key) => { return <option key= {key} value={c.id}>{c.categoryName}</option>})}
+              <select size="1" name="categoryId" onClick={handleChange}>
+                <option value="-2">Velg kategori</option>
+              {categories.map((c, key) => { return <option key= {key} value={c.id}>{c.categoryName}</option>})}
             </select>
 
              <input type="submit" value="Post" />
       </form>
-);
+      </div>
+    );
 }
