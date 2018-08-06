f0fc4016caa9cc90cb24fa3d08c16a89d98bf833
Christoffer Johnsen <christoffer@getacademy.no>
2018-08-06 11:07:42 +0200

    Fikk ikke med meg PatchAnswer i forrige push...

--- 24SevenOfficeForum/Models/PatchAnswer.cs
+++ 24SevenOfficeForum/Models/PatchAnswer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace _24SevenOfficeForum.Models
+﻿namespace _24SevenOfficeForum.Models
 {
     public class PatchAnswer
     {
