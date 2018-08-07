5a01cbd8d43f1bfcfe2f43956e25cf79d8c2a876
Christoffer Johnsen <christoffer@getacademy.no>
2018-08-07 15:45:11 +0200

    Forbedret søke funksjon./api/search?id=tekst&searchInAnswer=true&searchInCategory=false

--- 24SevenOfficeForum/Controllers/SearchController.cs
+++ 24SevenOfficeForum/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using _24SevenOfficeForum.Models;
 
 namespace _24SevenOfficeForum.Controllers
@@ -18,16 +19,15 @@ namespace _24SevenOfficeForum.Controllers
 		//GET: api/Search
 		[HttpGet]
 		[Produces("application/json")]
-		public IQueryable<Question> GetSearch(string id)
+		public IQueryable<Question> GetSearch([FromQuery]string id, bool searchInAnswer, bool searchInCategory)
 		{
 			string searchString = id;
-
-			var search = _context.Question.Where(m =>
-			m.Body.Contains(searchString) || m.Header.Contains(searchString) ||
-			m.Answer.Any(a => a.Body.Contains(searchString)) ||
-			m.Category.Description.Contains(searchString) ||
-			m.Category.CategoryName.Contains(searchString));
-
+			var search = from m in _context.Question.Include(z => z.Category).Include(z => z.Answer)
+				where m.Body.Contains(searchString) ||
+				      m.Header.Contains(searchString) ||
+				      searchInAnswer && m.Answer.Any(a => a.Body.Contains(searchString)) ||
+				      searchInCategory && m.Category.Description.Contains(searchString) || m.Category.CategoryName.Contains(searchString)
+				select m;
 			return search;
 		}
 	}
