9666a556439db09f0b3b31507ec39055a1dba591
hveidsten <hveidsten@gmail.com>
2018-08-16 09:52:59 +0200

    no message

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { connect} from 'react-redux';
+import { connect } from 'react-redux';
 import './App.css';
 
-import {Header} from './Components/Header/Header';
+import { Header } from './Components/Header/Header';
 import Home from './Components/Home/HomeContainer';
 import Sidebar from './Components/Sidebar/Sidebar';
 import NewQuestionContainer from './Components/NewQuestion/NewQuestionContainer';
@@ -11,57 +11,57 @@ import NewQuestionContainer from './Components/NewQuestion/NewQuestionContainer'
 import QuestionsList from './Components/QuestionList/QuestionsList';
 import SingleQuestion from './Components/SingleQuestion/SingleQuestion'
 
-import {fetchAllCategories, fetchSingleCategory} from './Actions/categoryActions';
+import { fetchAllCategories, fetchSingleCategory } from './Actions/categoryActions';
 
 
 class App extends Component {
 
-componentDidMount(){
- this.props.fetchAllCategories();
-}
+  componentDidMount() {
+    this.props.fetchAllCategories();
+  }
 
 
   render() {
     return (
       <BrowserRouter>
-      
-      <div className="App">
-        <Header />
-        <Sidebar />
-        <div className="Content" >
-        <Switch>
-        <Route exact path="/" component={Home}/>
 
-{this.props.categories && this.props.categories.map(
-  (c, key) => {
-    return <Route key={key} exact path={"/"+c.categoryName.replace(' ','_')} 
-    render={(props) => <QuestionsList activeCategory={c.id}  {...props} />}/>
-    }
-  )
-}
+        <div className="App">
+          <Header />
+          <Sidebar />
+          <div className="Content" >
+            <Switch>
+              <Route exact path="/" component={Home} />
 
-<Route exact path="/sok/:kat/:searchQuery"  component={QuestionsList}/>
-<Route exact path="/:categoryid/:questionid" component={SingleQuestion}/>
-<Route exact path="/new_question" component={NewQuestionContainer} />
-<Route exact path="/edit_question" component={NewQuestionContainer} />
+              {this.props.categories && this.props.categories.map(
+                (c, key) => {
+                  return <Route key={key} exact path={"/" + c.categoryName.replace(' ', '_')}
+                    render={(props) => <QuestionsList activeCategory={c.id}  {...props} />} />
+                }
+              )
+              }
 
-</Switch>
-</div>
-      </div>
+              <Route exact path="/sok/:kat/:searchQuery" component={QuestionsList} />
+              <Route exact path="/:categoryid/:questionid" component={SingleQuestion} />
+              <Route exact path="/new_question" component={NewQuestionContainer} />
+              <Route exact path="/edit_question" component={NewQuestionContainer} />
+
+            </Switch>
+          </div>
+        </div>
       </BrowserRouter>
     );
   }
-  
+
 }
 
-function mapStateToProps(state) { 
+function mapStateToProps(state) {
 
   return {
-    	categories: state.category.allCategories
-    };
-    
-} 
+    categories: state.category.allCategories
+  };
 
-export default connect( 
-  mapStateToProps, {fetchSingleCategory,fetchAllCategories}
+}
+
+export default connect(
+  mapStateToProps, { fetchSingleCategory, fetchAllCategories }
 )(App);
