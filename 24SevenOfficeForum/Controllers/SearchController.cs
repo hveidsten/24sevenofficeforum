2157bcdcf296b786a8d81dd5509a9df3ac7e7ce8
Christoffer Johnsen <christoffer@getacademy.no>
2018-08-24 07:27:35 +0200

    Sort and Paging on Search..

--- 24SevenOfficeForum/Controllers/SearchController.cs
+++ 24SevenOfficeForum/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 using _24SevenOfficeForum.Models;
 
 namespace _24SevenOfficeForum.Controllers
@@ -19,15 +20,34 @@ namespace _24SevenOfficeForum.Controllers
 		//GET: api/Search
 		[HttpGet]
 		[Produces("application/json")]
-		public IQueryable<Question> Search([FromQuery]string id, bool searchInAnswer, bool searchInCategory)
+		public async System.Threading.Tasks.Task<IQueryable<Question>> SearchAsync([FromQuery]string id, bool searchInAnswer, bool searchInCategory, string sortOrder, int? page)
 		{
 			string searchString = id;
 			var search = from question in _context.Question.Include(q => q.Category).Include(q => q.Answer)
 				where question.Body.Contains(searchString) ||
 				      question.Header.Contains(searchString) ||
 				      searchInAnswer && question.Answer.Any(a => a.Body.Contains(searchString)) ||
-				      searchInCategory && question.Category.Description.Contains(searchString) || question.Category.CategoryName.Contains(searchString)
+				      searchInCategory && question.Category.Description.Contains(searchString) ||
+				      question.Category.CategoryName.Contains(searchString)
 				select question;
+
+			if (searchString != null)
+			{
+				if (sortOrder == "created_asc") search = search.OrderBy(x => x.QuestionCreated);
+				else if (sortOrder == "vote_asc") search = search.OrderBy(x => x.Upvote);
+				else if (sortOrder == "vote_desc") search = search.OrderByDescending(x => x.Upvote);
+				else if (sortOrder == "count_asc") search = search.OrderBy(x => x.AnswerCount);
+				else if (sortOrder == "count_desc") search = search.OrderByDescending(x => x.AnswerCount);
+				else search = search.OrderByDescending(x => x.QuestionCreated);
+
+				if (page == null) page = 1;
+				int pageSize = 10;
+				int skipRows = (page.Value - 1) * pageSize;
+				  await search
+					.Skip(skipRows)
+					.Take(pageSize)
+					.AsNoTracking().ToListAsync();
+			}
 			return search;
 		}
 	}
